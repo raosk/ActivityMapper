81d0187b9bc1fc1e68c1dae255e9707c4ef8bd48
Selina Kallio <selina.kallio@gmail.com>
2022-04-06 13:17:49 +0300

    base for activityview, graphical details

--- public/scripts/previewBoxes.js
+++ public/scripts/previewBoxes.js
@@ -1,26 +1,26 @@
 function previewBoxesMain() {
     let displayedText = "";
-    for (let i = 1; i < dummyData.length; i++) {
-        if (dummyData[i].description.length > 30){
-            displayedText = dummyData[i].description.substring(0,30);
+    for (let i = 0; i < dummyData.length; i++) {
+        if (dummyData[i].description.length > 50){
+            displayedText = dummyData[i].description.substring(0,50) + "...";
         }else {
             displayedText = dummyData[i].description;
         }
         console.log(displayedText);
         
-        document.getElementById("previewsTarget").innerHTML = 
+        let previewBox = document.createElement("div");
+        previewBox.innerHTML =
         `
             <div class="preview_box_container">
                 <div class="activity_preview_box_image_container"> 
-                    <img class="activity_preview_box_image" src="/images/preview/music.png">
-                    
-                    <p class="activity_preview_box_title">${dummyData[i].title}</p>
-                    <p class="activity_preview_box_description">${dummyData[i].area} </p>
+                    <img class="activity_preview_box_image" src="/images/preview/music.png">                   
+                    <div class="activity_preview_box_title">${dummyData[i].title}</div>
+                    <div class="activity_preview_box_description">${displayedText} </div>
                 </div>
 
                 <div class="preview_box">
-                    <p class="activity_preview_box_title"> Area: ${dummyData[i].area}</p>
-                    <p class="activity_preview_box_description"> Time: ${dummyData[i].dateStart} -  ${dummyData[i].dateEnd}</p>
+                    <p class="activity_preview_box_area"> Area: ${dummyData[i].area}</p>
+                    <p class="activity_preview_box_time"> Time: ${dummyData[i].dateStart} -  ${dummyData[i].dateEnd}</p>
                 </div>
                 <hr size = "4" width = "100%" color = "black">
             </div>
@@ -28,13 +28,15 @@ function previewBoxesMain() {
         if (dummyData[i].dateStart == dummyData[i].dateEnd){
             document.querySelectorAll("preview_box")[i].innerHTML = 
             `
-            <p class="activity_preview_box_title"> Area: ${dummyData[i].area}</p>
-            <p class="activity_preview_box_description"> Time: ${dummyData[i].dateStart}</p>
+            <p class="activity_preview_box_area"> Area: ${dummyData[i].area}</p>
+            <p class="activity_preview_box_time"> Time: ${dummyData[i].dateStart}</p>
             `
         }
+        
+        document.getElementById("previewsTarget").appendChild(previewBox)
     }
 }
-
+/* 
 function previewBoxesResults() {
     document.getElementById("previewsTarget").innerHTML = `
         <div class="preview_box_container">
@@ -45,4 +47,4 @@ function previewBoxesResults() {
             </div>
         </div>
     `
-}
+} */
