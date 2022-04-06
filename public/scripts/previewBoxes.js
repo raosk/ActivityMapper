df5c4a17b81d663762a21fa05d8d235b8b8618fa
Selina Kallio <selina.kallio@gmail.com>
2022-04-06 10:59:10 +0300

    bug fixes and editing to queryselector

--- public/scripts/previewBoxes.js
+++ public/scripts/previewBoxes.js
@@ -1,16 +1,18 @@
 function previewBoxesMain() {
     let displayedText = "";
     for (let i = 1; i < dummyData.length; i++) {
-        if (dummydata[i].description.length > 30){
-            displayedText = dummydata[i].description.substring(0,30);
+        if (dummyData[i].description.length > 30){
+            displayedText = dummyData[i].description.substring(0,30);
         }else {
-            displayedText = dummydata[i].description;
+            displayedText = dummyData[i].description;
         }
+        console.log(displayedText);
         
-        document.getElementById("previewsTarget").innerHTML = `
+        document.getElementById("previewsTarget").innerHTML = 
+        `
             <div class="preview_box_container">
                 <div class="activity_preview_box_image_container"> 
-                    <img class="activity_preview_box_image" src="/images/music.png">
+                    <img class="activity_preview_box_image" src="/images/preview/music.png">
                     
                     <p class="activity_preview_box_title">${dummyData[i].title}</p>
                     <p class="activity_preview_box_description">${dummyData[i].area} </p>
@@ -24,9 +26,10 @@ function previewBoxesMain() {
             </div>
         `
         if (dummyData[i].dateStart == dummyData[i].dateEnd){
-            document.querySelectorAll("preview_box")[i].childNodes[1].innerHTML = 
+            document.querySelectorAll("preview_box")[i].innerHTML = 
             `
-                <p> Time: ${dummyData[i].dateStart}</p>
+            <p class="activity_preview_box_title"> Area: ${dummyData[i].area}</p>
+            <p class="activity_preview_box_description"> Time: ${dummyData[i].dateStart}</p>
             `
         }
     }
@@ -34,12 +37,12 @@ function previewBoxesMain() {
 
 function previewBoxesResults() {
     document.getElementById("previewsTarget").innerHTML = `
-    <div class="preview_box_container">
-        <div class="preview_box">
-            <p>${dummyData[3].title}</p>
-            <p>${dummyData[3].area} </p>
-            <p>${dummyData[3].dateStart} -  ${dummyData[1].dateEnd}</p>
+        <div class="preview_box_container">
+            <div class="preview_box">
+                <p>${dummyData[3].title}</p>
+                <p>${dummyData[3].area} </p>
+                <p>${dummyData[3].dateStart} -  ${dummyData[1].dateEnd}</p>
+            </div>
         </div>
-    </div>
     `
 }
