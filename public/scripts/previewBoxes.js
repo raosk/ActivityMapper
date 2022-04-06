e8954eec69f5c2ee7dffade07506fc46ad78b41c
Selina Kallio <selina.kallio@gmail.com>
2022-04-06 10:42:30 +0300

    creating base for preview boxes

--- public/scripts/previewBoxes.js
+++ public/scripts/previewBoxes.js
@@ -1,23 +1,35 @@
 function previewBoxesMain() {
-    document.getElementById("previewsTarget").innerHTML = `
-    <div class="preview_box_container">
-        <div class="preview_box">
-            <p>${dummyData[0].title}</p>
-            <p>${dummyData[0].area} </p>
-            <p>${dummyData[0].dateStart} -  ${dummyData[0].dateEnd}</p>
-        </div>
-        <div class="preview_box">
-            <p>${dummyData[1].title}</p>
-            <p>${dummyData[1].area} </p>
-            <p>${dummyData[1].dateStart} -  ${dummyData[1].dateEnd}</p>
-        </div>
-        <div class="preview_box">
-            <p>${dummyData[2].title}</p>
-            <p>${dummyData[2].area} </p>
-            <p>${dummyData[2].dateStart} -  ${dummyData[2].dateEnd}</p>
-        </div>
-    </div>
-    `
+    let displayedText = "";
+    for (let i = 1; i < dummyData.length; i++) {
+        if (dummydata[i].description.length > 30){
+            displayedText = dummydata[i].description.substring(0,30);
+        }else {
+            displayedText = dummydata[i].description;
+        }
+        
+        document.getElementById("previewsTarget").innerHTML = `
+            <div class="preview_box_container">
+                <div class="activity_preview_box_image_container"> 
+                    <img class="activity_preview_box_image" src="/images/music.png">
+                    
+                    <p class="activity_preview_box_title">${dummyData[i].title}</p>
+                    <p class="activity_preview_box_description">${dummyData[i].area} </p>
+                </div>
+
+                <div class="preview_box">
+                    <p class="activity_preview_box_title"> Area: ${dummyData[i].area}</p>
+                    <p class="activity_preview_box_description"> Time: ${dummyData[i].dateStart} -  ${dummyData[i].dateEnd}</p>
+                </div>
+                <hr size = "4" width = "100%" color = "black">
+            </div>
+        `
+        if (dummyData[i].dateStart == dummyData[i].dateEnd){
+            document.getElementById("preview_box").childNodes[1].innerHTML = 
+            `
+                <p> Time: ${dummyData[i].dateStart}</p>
+            `
+        }
+    }
 }
 
 function previewBoxesResults() {
@@ -31,4 +43,3 @@ function previewBoxesResults() {
     </div>
     `
 }
-//moi
