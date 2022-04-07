440ca60ba2bcf1e3304b417f8c0534d1107f8a89
Selina Kallio <selina.kallio@gmail.com>
2022-04-07 15:43:23 +0300

    providing 'tomorrow' and 'yesterday' labels in close days

--- public/scripts/previewBoxes.js
+++ public/scripts/previewBoxes.js
@@ -46,9 +46,26 @@ function checkDate() {
         }
         let today = new Date();
         let day = String(today.getDate()).padStart(2, '0');
-        let month = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let month = String(today.getMonth() + 1).padStart(2, '0'); //because january is 00
         let year = today.getFullYear();
 
+
+        let tomorrow = new Date(today);
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        let dayTomorrow = String(tomorrow.getDate()).padStart(2, '0');
+        let monthTomorrow = String(tomorrow.getMonth() + 1).padStart(2, '0');
+        let yearTomorrow = tomorrow.getFullYear();
+        
+        tomorrow = dayTomorrow + '.' + monthTomorrow + '.' + yearTomorrow;
+
+        let yesterday = new Date(today);
+        yesterday.setDate(yesterday.getDate() - 1);
+        let dayYesterday = String(yesterday.getDate()).padStart(2, '0');
+        let monthYesterday = String(yesterday.getMonth() + 1).padStart(2, '0');
+        let yearYesterday = yesterday.getFullYear();
+        
+        yesterday = dayYesterday + '.' + monthYesterday + '.' + yearYesterday;
+        
         today = day + '.' + month + '.' + year;
         if (dummyData[i].dateStart == dummyData[i].dateEnd && dummyData[i].dateStart == today){
             let previewBox = document.querySelectorAll(".preview_box")[i];
@@ -59,7 +76,27 @@ function checkDate() {
             <p class="activity_preview_box_time"> Time: Today</p>
             `;
             document.querySelectorAll(".preview_box_container")[i].appendChild(previewBox);
-        }  
+        }
+        if (dummyData[i].dateStart == dummyData[i].dateEnd && dummyData[i].dateStart == tomorrow){
+            let previewBox = document.querySelectorAll(".preview_box")[i];
+            document.querySelectorAll(".preview_box_container")[i].removeChild(previewBox)
+            previewBox.innerHTML = 
+            `
+            <p class="activity_preview_box_area"> Area: ${dummyData[i].area}</p>
+            <p class="activity_preview_box_time"> Time: Tomorrow</p>
+            `;
+            document.querySelectorAll(".preview_box_container")[i].appendChild(previewBox);
+        } 
+        if (dummyData[i].dateStart == dummyData[i].dateEnd && dummyData[i].dateStart == yesterday){
+            let previewBox = document.querySelectorAll(".preview_box")[i];
+            document.querySelectorAll(".preview_box_container")[i].removeChild(previewBox)
+            previewBox.innerHTML = 
+            `
+            <p class="activity_preview_box_area"> Area: ${dummyData[i].area}</p>
+            <p class="activity_preview_box_time"> Time: Yesterday</p>
+            `;
+            document.querySelectorAll(".preview_box_container")[i].appendChild(previewBox);
+        }       
     }
 }
 /* 
