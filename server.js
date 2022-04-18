3ab40407e39565d48923a6840e0ff8e1d0ddf6a9
raosk <oskari.rantamaula@gmail.com>
2022-04-18 16:27:54 +0300

    remote & local DB connections added

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const { Pool } = require('pg/lib');
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -8,5 +11,34 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.get("/db", async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = ( () => {
+        if (process.env.NODE_ENV !== 'production') {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: false
+            });
+        } else {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            });
+        }
+    })();
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT id, name, email FROM profile;');
+        const results = { 'results': (result) ? result.rows : null};
+        res.json( results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.json({ error: err });
+    }
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
