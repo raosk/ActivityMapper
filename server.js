b222f57cce8894ec91402e5ad21a99fa45f0d518
raosk <oskari.rantamaula@gmail.com>
2022-04-20 21:12:53 +0300

    Added separate routes folder/files & some test routes

--- server.js
+++ server.js
@@ -1,44 +1,19 @@
 require('dotenv').config();
-const { Pool } = require('pg/lib');
-
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// Static folder
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.get("/db", async (req, res) => {
-    const { Pool } = require('pg');
-    const pool = ( () => {
-        if (process.env.NODE_ENV !== 'production') {
-            return new Pool({
-                connectionString: process.env.DATABASE_URL,
-                ssl: false
-            });
-        } else {
-            return new Pool({
-                connectionString: process.env.DATABASE_URL,
-                ssl: {
-                    rejectUnauthorized: false
-                }
-            });
-        }
-    })();
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT id, name, email FROM profile;');
-        const results = { 'results': (result) ? result.rows : null};
-        res.json( results );
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.json({ error: err });
-    }
-});
+// Routes
+app.use('/profile', require('./routes/profile'));
+app.use('/search', require('./routes/search'));
+app.use('/', require('./routes/common'));
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
