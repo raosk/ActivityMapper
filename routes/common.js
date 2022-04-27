dcaee6b514fa8bde8d9ec266a2a0ea4bc2b6dc3f
raosk <oskari.rantamaula@gmail.com>
2022-04-27 16:02:22 +0300

    A batch of DB query routes added

--- routes/common.js
+++ routes/common.js
@@ -20,7 +20,6 @@ router.get("/db", async (req, res) => {
     })();
     try {
         const client = await pool.connect();
-        /* const result = await client.query('SELECT * FROM activity;'); */     // This is for the actual DB table
         const result = await client.query('SELECT id, name, email FROM profile;');  // This is for the test table, which will be removed
         const results = { 'results': (result) ? result.rows : null};
         res.json( results );
@@ -30,6 +29,164 @@ router.get("/db", async (req, res) => {
         res.json({ error: err });
     }
 });
+
+router.get("/new", async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = ( () => {
+        if (process.env.NODE_ENV !== 'production') {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: false
+            });
+        } else {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            });
+        }
+    })();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(`
+        select * from activity
+        inner join feed on activity.activity_id=feed.feed_id
+        inner join users on feed.user_user_id=user_id
+        inner join activity_has_category on activity.activity_id=activity_has_category.activity_activity_id
+        inner join category on activity_has_category.category_category_id=category.category_id	
+        order by date_created ASC 
+        limit 20
+        `);
+        // const result = await client.query('SELECT id, name, email FROM profile;');  // This is for the test table, which will be removed
+        const results = { 'results': (result) ? result.rows : null};
+        res.json( results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.json({ error: err });
+    }
+});
+
+router.get("/popular", async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = ( () => {
+        if (process.env.NODE_ENV !== 'production') {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: false
+            });
+        } else {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            });
+        }
+    })();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(`
+        select description, count(*) as done from activity
+        inner join feed on activity.activity_id="feed.activity_activityId"
+        inner join users on feed.user_user_id=users.user_id
+        inner join activity_has_category on activity.activity_id=activity_activity_id
+        inner join category on category_category_id=category_id
+        where done = '1' and to_do = '0'
+        group by description
+        order by done desc
+        limit 20;
+        `);
+        // const result = await client.query('SELECT id, name, email FROM profile;');  // This is for the test table, which will be removed
+        const results = { 'results': (result) ? result.rows : null};
+        res.json( results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.json({ error: err });
+    }
+});
+
+router.get("/upcoming", async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = ( () => {
+        if (process.env.NODE_ENV !== 'production') {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: false
+            });
+        } else {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            });
+        }
+    })();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(`
+        select * from activity
+        inner join feed on activity.activity_id=feed.feed_id
+        inner join users on feed.user_user_id=user_id
+        inner join activity_has_category on activity.activity_id=activity_has_category.activity_activity_id
+        inner join category on activity_has_category.category_category_id=category.category_id	
+        where date_start between '2022-04-21' and '2022-12-30'
+        and date_end between '2022-04-21' and '2022-12-30'
+        order by date_start asc
+        limit 20;
+        `);
+        // const result = await client.query('SELECT id, name, email FROM profile;');  // This is for the test table, which will be removed
+        const results = { 'results': (result) ? result.rows : null};
+        res.json( results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.json({ error: err });
+    }
+});
+
+router.get("/past", async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = ( () => {
+        if (process.env.NODE_ENV !== 'production') {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: false
+            });
+        } else {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            });
+        }
+    })();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(`
+        select * from activity
+        inner join feed on activity.activity_id=feed.feed_id
+        inner join users on feed.user_user_id=user_id
+        inner join activity_has_category on activity.activity_id=activity_has_category.activity_activity_id
+        inner join category on activity_has_category.category_category_id=category.category_id	
+        where date_end between '2020-04-21' and '2022-04-21'
+        order by date_start asc
+        limit 20;
+        `);
+        // const result = await client.query('SELECT id, name, email FROM profile;');  // This is for the test table, which will be removed
+        const results = { 'results': (result) ? result.rows : null};
+        res.json( results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.json({ error: err });
+    }
+});
+
 
 // TESTING: START
 router.get("/banana", (req, res) => {
--- routes/profile.js
+++ routes/profile.js
@@ -5,4 +5,78 @@ router.get('/', (req, res) => {
     res.send("No profile things yet");
 });
 
+router.get("/todo", async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = ( () => {
+        if (process.env.NODE_ENV !== 'production') {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: false
+            });
+        } else {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            });
+        }
+    })();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(`
+        select * from activity
+        inner join feed on activity.activity_id=feed."activity_activityId"
+        inner join users on feed.user_user_id=users.user_id
+        inner join activity_has_category on activity.activity_id=activity_activity_id
+        inner join category on category_category_id=category_id
+        where to_do = '1' and done = '0' and user_name='käyttäjä1'
+        `);
+        // const result = await client.query('SELECT id, name, email FROM profile;');  // This is for the test table, which will be removed
+        const results = { 'results': (result) ? result.rows : null};
+        res.json( results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.json({ error: err });
+    }
+});
+
+router.get("/done", async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = ( () => {
+        if (process.env.NODE_ENV !== 'production') {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: false
+            });
+        } else {
+            return new Pool({
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            });
+        }
+    })();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(`
+        select * from activity
+        inner join feed on activity.activity_id=feed."activity_activityId"
+        inner join users on feed.user_user_id=users.user_id
+        inner join activity_has_category on activity.activity_id=activity_activity_id
+        inner join category on category_category_id=category_id
+        where done = '1' and to_do ='0' and user_name='käyttäjä1'
+        `);
+        // const result = await client.query('SELECT id, name, email FROM profile;');  // This is for the test table, which will be removed
+        const results = { 'results': (result) ? result.rows : null};
+        res.json( results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.json({ error: err });
+    }
+});
+
 module.exports = router;
