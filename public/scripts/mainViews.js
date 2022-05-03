a2172d25cc0a974e4d1ddc1856d5d959c6fc98a9
Eettee12 <90237451+Eettee12@users.noreply.github.com>
2022-05-03 21:54:24 +0300

    Re-ordering the icons

--- public/scripts/mainViews.js
+++ public/scripts/mainViews.js
@@ -4,14 +4,14 @@ function viewBase() {
     <div class="navbar">
         <a href="#"><img class="logo-img" img src="images/icons/thick_logo-modified.png" alt="#" width="50px" height="50px"></a>
         <ul class="desktop-list">
-        <li><i class="fa-solid fa-user" onclick="isLoggedIn()"></i></li>
-        <li><i class="fa-solid fa-globe" onclick="isLoggedIn()"></i></li>
         <li><i class="fa-solid fa-magnifying-glass" onclick="searchPopUp()"></i></li>
+        <li><i class="fa-solid fa-globe" onclick="isLoggedIn()"></i></li>
+        <li><i class="fa-solid fa-user" onclick="isLoggedIn()"></i></li>
         </ul>
         <ul class="mobile-list">
-            <li><i class="fa-solid fa-user" onclick="isLoggedIn()"></i></li>
-            <li><i class="fa-solid fa-globe" onclick="isLoggedIn()"></i></li>
-            <li><i class="fa-solid fa-magnifying-glass" onclick="searchPopUp()"></i></li>
+        <li><i class="fa-solid fa-magnifying-glass" onclick="searchPopUp()"></i></li>
+        <li><i class="fa-solid fa-globe" onclick="isLoggedIn()"></i></li>
+        <li><i class="fa-solid fa-user" onclick="isLoggedIn()"></i></li>
         </ul>
     </div>
 
